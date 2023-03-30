9fdf3efc0d862958c2f8b2751bfc5591646e2f39
LooanCheong <jjyejun@naver.com>
2023-03-30 21:46:29 +0900

    채팅 기능 연습

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -15,11 +15,23 @@ const addMessage = (msg) => {
   ul.appendChild(li);
 };
 
+const handleMessageSubmit = (e) => {
+  e.preventDefault();
+  const input = room.querySelector("input");
+  const value = input.value;
+  socket.emit("new_message", input.value, roomName, () => {
+    addMessage(`You: ${value}`);
+  });
+  input.value = "";
+};
+
 const showRoom = () => {
   welcome.hidden = true;
   room.hidden = false;
   const h3 = room.querySelector("h3");
   h3.innerText = `Room ${roomName}`;
+  const form = room.querySelector("form");
+  form.addEventListener("submit", handleMessageSubmit);
 };
 
 const handleRoomSubmit = (e) => {
@@ -35,3 +47,9 @@ form.addEventListener("submit", handleRoomSubmit);
 socket.on("welcome", () => {
   addMessage("Someone Joined!");
 });
+
+socket.on("bye", () => {
+  addMessage("Someone left!");
+});
+
+socket.on("new_message", addMessage);
